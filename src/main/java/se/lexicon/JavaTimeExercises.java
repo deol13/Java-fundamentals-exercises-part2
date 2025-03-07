d3082f74da8d4ef33685a07545a20ef555311531
Dennis Olsen <dennan_lol@hotmail.com>
2025-03-07 13:24:06 +0100

    JavaTime exercises done

--- src/main/java/se/lexicon/JavaTimeExercises.java
+++ src/main/java/se/lexicon/JavaTimeExercises.java
@@ -1,4 +1,122 @@
 package se.lexicon;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class JavaTimeExercises {
+    public static void main(String[] args) {
+        // Exercise 1
+        System.out.println("Exercise 1: ");
+
+        LocalDate ex1Date = LocalDate.now();
+        System.out.println(ex1Date);
+
+        System.out.println("---------------");
+        // Exercise 2
+        System.out.println("Exercise 2: ");
+
+        LocalDate ex2Date = LocalDate.now();
+        String ex2DateFormatted = ex2Date.format(DateTimeFormatter.ofPattern("eeee dd MMM"));
+        System.out.println(ex2DateFormatted);
+
+        System.out.println("---------------");
+        // Exercise 3
+        System.out.println("Exercise 3: ");
+
+        //LocalDate.now().minus(5);
+        LocalDate ex3Date = LocalDate.parse("2025-03-03");//LocalDate.of(2025, 3, 1);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(ex3Date.format(DateTimeFormatter.ISO_DATE));
+            ex3Date = ex3Date.plusDays(1);
+        }
+
+        System.out.println("---------------");
+        // Exercise 4
+        System.out.println("Exercise 4: ");
+        LocalDate ex4Date = LocalDate.parse("2024-12-01");
+        System.out.println("---------------");
+        // Exercise 5
+        System.out.println("Exercise 5: ");
+
+        LocalDate birthDate = LocalDate.parse("1993-07-04");
+        System.out.println(birthDate.format(DateTimeFormatter.ISO_DATE) + " -> " + birthDate.getDayOfWeek());
+
+        System.out.println("---------------");
+        // Exercise 6
+        System.out.println("Exercise 6: ");
+
+        LocalDate ex6Date = LocalDate.now().minusYears(10).minusMonths(10);
+        System.out.println("Month: " + ex6Date.getMonth());
+
+        System.out.println("---------------");
+        // Exercise 7
+        System.out.println("Exercise 7: ");
+        Period periodBetween = Period.between(birthDate, ex6Date);
+        System.out.println("Years: " + periodBetween.getYears());
+        System.out.println("Months: " + periodBetween.getMonths());
+        System.out.println("Days: " + periodBetween.getDays());
+        System.out.println("---------------");
+        // Exercise 8
+        System.out.println("Exercise 8: ");
+        Period ex8Period = Period.parse("P4Y7M29D");
+        LocalDate ex8Date = LocalDate.now();
+        LocalDate futureDate = ex8Date.plus(ex8Period);
+        System.out.println("Future day of 4y, 9m and 29d: " + futureDate.format(DateTimeFormatter.ISO_DATE));
+
+        System.out.println("---------------");
+        // Exercise 9
+        System.out.println("Exercise 9: ");
+        LocalTime ex9Time = LocalTime.now();
+        System.out.println("Time: " + ex9Time);
+
+        System.out.println("---------------");
+        // Exercise 10
+        System.out.println("Exercise 10: ");
+        LocalTime ex10Time = LocalTime.now();
+        System.out.println("Time.now's nanoseconds: " + ex10Time.getNano());
+
+        System.out.println("---------------");
+        // Exercise 11
+        System.out.println("Exercise 11: ");
+        LocalTime ex11Time = LocalTime.parse("13:37:00");
+        System.out.println("Parsed time : " + ex11Time);
+        System.out.println("---------------");
+        // Exercise 12
+        System.out.println("Exercise 12: "); // Not sure what's going on about the pattern the exercise want.
+        LocalTime ex12Time = LocalTime.now();
+        System.out.println("current time: " + ex12Time.truncatedTo(ChronoUnit.SECONDS).format(DateTimeFormatter.ISO_LOCAL_TIME));
+        System.out.println("---------------");
+        // Exercise 13
+        System.out.println("Exercise 13: ");
+        LocalDateTime ex13DateTime = LocalDateTime.parse("2018-04-05T10:00");
+        System.out.println(ex13DateTime);
+        System.out.println("---------------");
+        // Exercise 14
+        System.out.println("Exercise 14: ");
+        System.out.println(ex13DateTime.format(DateTimeFormatter.ofPattern("eeee dd MMM H:mm")));
+        System.out.println("---------------");
+        // Exercise 15
+        System.out.println("Exercise 15: ");
+        LocalDate ex15Date = LocalDate.now();
+        LocalTime ex15time = LocalTime.now();
+
+        LocalDateTime ex15DateTime = LocalDateTime.parse(ex15Date + "T" + ex15time.truncatedTo(ChronoUnit.MINUTES));
+        System.out.println("LocalDateTime:" + ex15DateTime);
+        System.out.println("---------------");
+        // Exercise 16
+        System.out.println("Exercise 16: ");
+        LocalDateTime ex16DateTime = LocalDateTime.now();
+        LocalDate ex16Date = ex16DateTime.toLocalDate();
+        LocalTime ex16Time = ex16DateTime.toLocalTime();
+        System.out.println("Date: " + ex16Date.format(DateTimeFormatter.ISO_DATE));
+        System.out.println("Time: " + ex16Time.format(DateTimeFormatter.ISO_TIME));
+        System.out.println("---------------");
+
+        // Exercise extra
+
+    }
 }
--- src/main/java/se/lexicon/StringExercises.java
+++ src/main/java/se/lexicon/StringExercises.java
@@ -1,4 +1,78 @@
 package se.lexicon;
 
+import java.util.Arrays;
+
 public class StringExercises {
+    public static void main(String[] args) {
+        // Exercise 1: length of the String "Java" is 4.
+        String str = "Java";
+        System.out.println(str.length());
+
+        // Exercise 2: The 6th index in the string "Long example sentence" is x
+        String str2 = "Long example sentence";
+        System.out.println(str2.charAt(6));
+
+        // Exercise 2.5: o in Even "longer example sentence" has the index 6
+        String str25 = "Even longer example sentence";
+        System.out.println(str25.indexOf("o"));
+
+        // Exercise 3
+        String str3 = "Ok this is not as long";
+        System.out.println(str3.substring(11, 22));
+
+        // Exercise 4
+        String str4 = "CAPS EQUALS SCREAMING";
+        String str4LowerCase = str4.toLowerCase();
+        System.out.println(str4LowerCase);
+        String str4UpperCase = str4LowerCase.toUpperCase();
+        System.out.println(str4UpperCase);
+
+        // Exercise 5
+        String str5 = "Java is the worst programming language!";
+        String str5CorrectVersion = str5.replace("worst", "best");
+        System.out.println(str5CorrectVersion);
+
+        // Exercise 6 the output of "\tJ\ta\tv\ta\t" after trimming it is "J  a  v  a"
+        String str6 = "\tJ\ta\tv\ta\t";
+        System.out.println(str6.trim());
+
+        // Exercise 7
+        int exer7 = 20;
+        String str7 = String.valueOf(exer7);
+        System.out.println("String after parsing int to it: " + str7);
+        str7 = str7.concat("20");
+        System.out.println("String after adding 20 to it: " + str7);
+
+        // Exercise 8
+        String str8 = "Oil and Water";
+        String[] str8Arr = str8.split("and");
+        System.out.println("Oil: " + str8Arr[0]);
+        System.out.println("Water: " + str8Arr[1].trim());
+
+        // Exercise 9
+        String str9 = "Carl,Susie,Fredrick,Bob,Erik";
+        String[] str9NameArr = str9.split(",");
+        for (String item : str9NameArr){
+            System.out.println(item);
+        }
+
+        // Exercise 10
+        String str10 = "ThisShouldBeConverted";
+        char[] str10CharArr = str10.toCharArray();
+        for (char character : str10CharArr){
+            System.out.println(character);
+        }
+
+        // Exercise 11
+        char[] exercise11CharArr = {'J','a','v','a'};
+
+        String str11 = new String(exercise11CharArr);
+        System.out.println("Creating a new string and adding the array as a argument to the constructor: " + str11);
+
+        String str11ValueOf = String.valueOf(exercise11CharArr);
+        System.out.println("String.valueOf: " + str11ValueOf);
+
+
+
+    }
 }
